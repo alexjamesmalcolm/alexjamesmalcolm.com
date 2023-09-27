5ad633ac5a3788e16df3268e928bc604f6176c8f
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2023-09-27 01:05:28 -0400

    Made some progress on filling out the data for the resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -3,25 +3,71 @@ import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 // Create styles
 const styles = StyleSheet.create({
   page: {
-    flexDirection: "row",
-    backgroundColor: "#E4E4E4",
+    // flexDirection: "row",
   },
   section: {
     margin: 10,
     padding: 10,
     flexGrow: 1,
   },
+  header: {
+    fontSize: "20px",
+    textDecoration: "underline",
+  },
 });
+
+interface WorkExperienceParams {
+  companyName: string;
+  jobTitle: string;
+}
+function WorkExperience({
+  companyName,
+  jobTitle,
+}: WorkExperienceParams): JSX.Element {
+  return (
+    <View>
+      <Text>{companyName}</Text>
+      <Text>{jobTitle}</Text>
+    </View>
+  );
+}
+
+interface VolunteerExperienceParams {}
+function VolunteerExperience({}: VolunteerExperienceParams): JSX.Element {
+  return <View></View>;
+}
+
+const workExperiences: WorkExperienceParams[] = [
+  { companyName: "AEP", jobTitle: "Software Developer" },
+  { companyName: "Everest Technologies", jobTitle: "Software Developer" },
+  { companyName: "PNC", jobTitle: "Software Developer" },
+];
+const volunteerExperiences: VolunteerExperienceParams[] = [];
 
 export default function Resume() {
   return (
     <Document>
       <Page size="A4" style={styles.page}>
         <View style={styles.section}>
-          <Text>Section #1</Text>
+          <Text>Alex Malcolm</Text>
+          <Text>55 W Hudson St, Unit B</Text>
+          <Text>Columbus, OH, 43202</Text>
+          <Text>(614) 512-9587</Text>
+          <Text>alexjamesmalcolm@gmail.com</Text>
         </View>
         <View style={styles.section}>
-          <Text>Section #2</Text>
+          <Text style={styles.header}>Objective</Text>
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.header}>Education</Text>
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.header}>Work Experience</Text>
+          {workExperiences.map(WorkExperience)}
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.header}>Volunteer Experience</Text>
+          {volunteerExperiences.map(VolunteerExperience)}
         </View>
       </Page>
     </Document>
